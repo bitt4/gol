5efdd69f546e933ef01e36d3adf6f19cfc3cf64b
bitt4 <jakub.bittara@gmail.com>
2020-10-15 21:56:36 +0200

    Functions, members, etc.

--- src/Gol.hpp
+++ src/Gol.hpp
@@ -1,11 +1,16 @@
 #include <SDL2/SDL.h>
+#include <cstdlib>
 
 class Gol {
 private:
     int width, height;
-    bool* first_grid, second_grid;
+    int cell_width;
+    bool* first_grid;
+    bool* second_grid;
+
+    int get_nearby_cells(int x, int y);
 public:
-    Gol(int width, int height, bool* initial_state);
+    Gol(int width, int height, int cell_width, bool* initial_state);
     ~Gol();
     void update();
     void render();
