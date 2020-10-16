50506122b4a9874c5c7d163b68fe596a01f939b7
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-10-16 20:25:04 +0200

    Implement draw_cell, update and render functions
    get_nearby_cells is still unimplemented

--- src/Gol.hpp
+++ src/Gol.hpp
@@ -9,10 +9,13 @@ private:
     bool* first_grid;
     bool* second_grid;
 
+    SDL_Color cell_color, background_color;
+
     int get_nearby_cells(int x, int y);
+    void draw_cell(SDL_Renderer* renderer, int x, int y, SDL_Color color);
 public:
     Gol(int width, int height, int cell_width, bool* initial_state);
     ~Gol();
     void update();
-    void render();
+    void render(SDL_Renderer* renderer);
 };
