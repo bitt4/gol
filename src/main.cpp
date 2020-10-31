514512e542e5e20265886f6c8b771d1df771fec1
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-10-31 23:59:59 +0100

    Start parsing files

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
@@ -9,6 +10,8 @@
 #include "Gol.hpp"
 
 long int parse_option_as_number(const char* arg, int max_length, bool allow_zero = false);
+
+void load_file(const char* filename, bool *&destination_grid);
 
 int main(int argc, char *argv[]){
 
@@ -79,7 +82,7 @@ int main(int argc, char *argv[]){
                     return_seed = true;
                 break;
             case 'c':
-                cell_size = parse_option_as_number(optarg, 3);    /* implicit cast from long to int */
+                cell_size = parse_option_as_number(optarg, 4);    /* implicit cast from long to int */
                 break;
             case 'v':
                 SPEED = parse_option_as_number(optarg, 4, true);  /* implicit cast from long to int */
@@ -223,4 +226,71 @@ long int parse_option_as_number(const char* arg, int max_length, bool allow_zero
     }
 
     return value;
+}
+
+void load_file(const char* filename, bool *&destination_grid){
+    /* For now handles simple files */
+    std::ifstream grid_file(filename);
+
+    int grid_width, grid_height = 1;
+
+    if(grid_file.is_open()){
+        /* Check if the file is empty, http://www.cplusplus.com/reference/ios/ios/eof/ */
+        if(grid_file.peek() == std::ifstream::traits_type::eof()){
+            fprintf(stderr, "File '%s' is empty.\n", filename);
+            exit(1);
+        }
+        else {
+            int max_line_width = 0;
+            int current_line_length = 0
+            
+            char c;
+            while(grid_file.get(c)){
+                if(c == '\n' && grid_file.peek() != std::ifstream::traits_type::eof()){
+                    grid_height++;
+                    max_line_width = MAX(max_line_width, current_line_length);
+                    current_line_length = 0;
+                }
+                else {
+                    current_line_length++;
+                }
+            }
+
+            grid_width = max_line_width;
+
+            /* allocate memory for grid */
+            int grid_size = grid_width * grid_height;
+            destination_grid = (bool*)calloc(grid_size, sizeof(bool));
+
+            /* fill the grid */
+            grid_file.clear();    /* Read file again from the beginning */
+            grid_file.seekg(0, grid_file.beg);
+
+            int grid_y = 0;
+            current_line_length = 0;
+
+            while(grid_file.get(c)){
+                if(c == '\n' && grid_file.peek() != std::ifstream::traits_type::eof()){
+                    grid_y++;
+                    current_line_length = 0;
+                }
+                else {
+                    current_line_length++;
+                    if(c == '0' || c == ' '){
+                        cell = false;
+                    }
+                    else {
+                        cell = true;
+                }
+                destination_grid[grid_y * grid_width + current_line_length - 1] = cell;
+                }
+                bool cell;
+                
+            }
+        }
+    }
+    else {
+        fprintf(stderr, "Can't open file '%s'.\n", filename);
+        exit(1);
+    }
 }
