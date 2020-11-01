0fa48da9acd8a27b28488629f38d60cf121b984e
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-11-01 15:51:14 +0100

    Move file parsing functions to another file
    Also fix some syntax errors

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,11 @@
 
 #include "Gol.hpp"
 
-long int parse_option_as_number(const char* arg, int max_length, bool allow_zero = false);
+#define MAX(a, b) ((a)>(b)?(a):(b))
 
-void load_file(const char* filename, bool *&destination_grid);
+#include "GameOfLifeParseFile.cpp"
+
+long int parse_option_as_number(const char* arg, int max_length, bool allow_zero = false);
 
 int main(int argc, char *argv[]){
 
@@ -209,7 +211,7 @@ long int parse_option_as_number(const char* arg, int max_length, bool allow_zero
             exit(1);
         }
     }
-    else if(value <= 0){       /* I don't think I want to allow negative values in any of the command-line arguments           */
+    else if(value <= 0){       /* I don't think I want to allow negative values in any of the command-line arguments */
         fprintf(stderr, "Invalid argument: %s is too small.\n", arg);
         exit(1);
     }
@@ -226,70 +228,4 @@ long int parse_option_as_number(const char* arg, int max_length, bool allow_zero
     }
 
     return value;
-}
-
-void load_file(const char* filename, bool *&destination_grid){
-    /* For now handles simple files */
-    std::ifstream grid_file(filename);
-
-    int grid_width, grid_height = 1;
-
-    if(grid_file.is_open()){
-        /* Check if the file is empty, http://www.cplusplus.com/reference/ios/ios/eof/ */
-        if(grid_file.peek() == std::ifstream::traits_type::eof()){
-            fprintf(stderr, "File '%s' is empty.\n", filename);
-            exit(1);
-        }
-        else {
-            int max_line_width = 0;
-            int current_line_length = 0
-            
-            char c;
-            while(grid_file.get(c)){
-                if(c == '\n' && grid_file.peek() != std::ifstream::traits_type::eof()){
-                    grid_height++;
-                    max_line_width = MAX(max_line_width, current_line_length);
-                    current_line_length = 0;
-                }
-                else {
-                    current_line_length++;
-                }
-            }
-
-            grid_width = max_line_width;
-
-            /* allocate memory for grid */
-            int grid_size = grid_width * grid_height;
-            destination_grid = (bool*)calloc(grid_size, sizeof(bool));
-
-            /* fill the grid */
-            grid_file.clear();    /* Read file again from the beginning */
-            grid_file.seekg(0, grid_file.beg);
-
-            int grid_y = 0;
-            current_line_length = 0;
-
-            while(grid_file.get(c)){
-                bool cell;
-                if(c == '\n'){
-                    grid_y++;
-                    current_line_length = 0;
-                }
-                else {
-                    if(c == '0' || c == ' '){
-                        cell = false;
-                    }
-                    else {
-                        cell = true;
-                    }
-                    destination_grid[grid_y * grid_width + current_line_length] = cell;
-                    current_line_length++;
-                }
-            }
-        }
-    }
-    else {
-        fprintf(stderr, "Can't open file '%s'.\n", filename);
-        exit(1);
-    }
 }
